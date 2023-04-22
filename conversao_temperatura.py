fda8daaf46b17b8ed6157b68b231b8aa560e0802
salfilho <salvador.filho@gmail.com>
2023-04-22 12:11:39 -0300

    converso de temperatura
    Converte temperatura em Celsius para Fahrenheit

---
+++ conversao_temperatura.py
@@ -0,0 +1,4 @@
+print ("Conversão de temperatura de Celsiu para Fahrenheit!")
+temperatura_celsius = float(input("Digite a temperatura em graus celsius: "))
+temperatura_fahrenheit = ( 9 * temperatura_celsius) / 5 + 32
+print ("A temperatura convertida em fahrenheit é de %5.2f" % temperatura_fahrenheit,"F")
